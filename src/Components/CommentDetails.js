dac9df9f7212ff1fde4a94ca778206051eb08d26
Anahid <anahidhemmat2130@gmail.com>
2022-07-08 18:22:50 +0400

    changes

--- src/Components/CommentDetails.js
+++ src/Components/CommentDetails.js
@@ -4,16 +4,16 @@ const CommentDetails = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img src={faker.image.avatar()} alt="avatar" />
+        <img src={props.avatar} alt="avatar" />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="data">Today at 6:00PM</span>
+          <span className="data">{props.time}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
