9fba67b1d74c431deade2c8ab7e7b6529de523c9
Anahid <anahidhemmat2130@gmail.com>
2022-07-08 19:53:32 +0400

    increment and decrement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import CommentDetails from "./Components/CommentDetails";
 import ApprovalCard from "./Components/ApprovalCard";
 import { faker } from "@faker-js/faker";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 const App = () => {
   const counter = useSelector((state) => state.counter);
   const isLogged = useSelector((state) => state.isLogged);
+
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>- </button>
 
       {isLogged ? <h3>Hidden info</h3> : ""}
     </div>
